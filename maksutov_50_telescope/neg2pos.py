d878080eab0ac3376f9f87e2d8148f5caa5121fe
Ildana Izmailova <ildana_izm@zah-151.zah.uni-heidelberg.de>
2022-11-22 11:11:03 +0100

    maksutov_50_telescope: A script to convert images from negative to positive is added

---
+++ maksutov_50_telescope/neg2pos.py
@@ -0,0 +1,15 @@
+"""
+Converts negative fits images to positive (and vice versa). Works correctly with files which data in range from 0 to 65,536.
+"""
+
+from astropy.io import fits
+import os
+
+list_dir = os.listdir()
+
+for f in list_dir:
+    if ".fits" in f:
+        #print(f)
+        hdul =  fits.open(f)
+        hdul[0].data = 65536 - hdul[0].data
+        hdul.writeto(f,overwrite=True)
