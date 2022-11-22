be850c17b1b55ada021f035d7274d73ccee63053
Ildana Izmailova <ildana_izm@zah-151.zah.uni-heidelberg.de>
2022-11-22 11:45:32 +0100

    maksutov_50_telescope: The script to convert negative to positive was fixed.
    Now the script shouldn't be in the same folder with frames, but it has to be in a folder back.

--- maksutov_50_telescope/neg2pos.py
+++ maksutov_50_telescope/neg2pos.py
@@ -5,6 +5,7 @@ Converts negative fits images to positive (and vice versa). Works correctly with
 from astropy.io import fits
 import os
 
+os.chdir("data")
 list_dir = os.listdir()
 
 for f in list_dir:
@@ -13,3 +14,4 @@ for f in list_dir:
         hdul =  fits.open(f)
         hdul[0].data = 65536 - hdul[0].data
         hdul.writeto(f,overwrite=True)
+	hdul.close()
