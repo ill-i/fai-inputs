773682e7969340a00512e02e8ebc345212f6ff3e
Ildana Izmailova <izmailova.ildana@gmail.com>
2022-11-28 16:10:38 +0100

    maksutov_50_telescope: Experimentally trying anet indexes with smaller quads.
    The hope is that this will be more robust against the wobble in these
    images.

--- maksutov_50_telescope/bin/annotate_fits.py
+++ maksutov_50_telescope/bin/annotate_fits.py
@@ -537,12 +537,12 @@ def defuse_international_string(s):
 
 
 class PAHeaderAdder(api.AnetHeaderProcessor):
-  indexPath = "/var/gavo/astrometry-indexes"
+  indexPath = "/usr/share/astrometry"
   sp_total_timelimit = 120
   sp_lower_pix = 2
   sp_upper_pix = 4
   sp_endob = 100
-  sp_indices = ["index-2mass-1[012].fits"]
+  sp_indices = ["index-2mass-07*.fits", "index-2mass-08*.fits"]
 
   sourceExtractorControl = """
     DETECT_MINAREA   20
