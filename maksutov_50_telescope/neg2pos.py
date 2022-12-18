4a757768716dfc2508e38f5cf0809068bf62c17f
Ildana Izmailova <izmailova.ildana@gmail.com>
2022-12-19 00:52:42 +0600

    maksutov_50_telescope: Fixed a mistake in converting process.
    In the last version we used "655536 - i" but now it is "65535 - i". As result files stay 16 bit and do not became 32 bit after converting.

--- maksutov_50_telescope/neg2pos.py
+++ maksutov_50_telescope/neg2pos.py
@@ -6,12 +6,14 @@ from astropy.io import fits
 import os
 
 os.chdir("data")
-list_dir = os.listdir()
 
-for f in list_dir:
-    if ".fits" in f:
-        #print(f)
-        hdul =  fits.open(f)
-        hdul[0].data = 65536 - hdul[0].data
-        hdul.writeto(f,overwrite=True)
+def convert_one(path):
+    hdul =  fits.open(path)
+    hdul[0].data = 65535 - hdul[0].data
+    hdul.writeto(path, overwrite=True)
 	hdul.close()
+
+
+for f_name in os.listdir():
+    if f_name.endswith(".fits"):
+        convert_one(f_name)
